92c3e99984233ca37e37d77f880025fa0c7875ef
Seungjae Moon <sjaymoon15@gmail.com>
2016-10-04 16:11:40 -0400

    react-fundamentals

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class App extends React.Component {
+	render(){
+		return (<div><div>Hello World</div><h1>second</h1></div>);
+	}
+}
+
+// const App = () => <h1>Hello Eggheads</h1>
+
+export default App
