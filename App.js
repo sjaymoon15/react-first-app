87dd0bada8790c8473b9017596d94967e4164d3c
Seungjae Moon <sjaymoon15@gmail.com>
2016-10-05 17:38:50 -0400

    props

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
 class App extends React.Component {
 	render(){
-		return (<div><div>Hello World</div><h1>second</h1></div>);
+		let txt = this.props.txt;
+		let cat = this.props.cat;
+		return <h1>{txt}{cat}</h1>;
 	}
 }
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
 
-// const App = () => <h1>Hello Eggheads</h1>
+App.defaultProps = {
+	txt: 'this is the default props',
+	cat: 10
+}
 
 export default App
